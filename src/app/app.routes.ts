0d8e15cee27aa439061864583aead6a5434fd2a6
IT-Misericordia-Chile <tichile@misericordia.fr>
2025-03-19 15:40:54 -0300

    add path redirection to login when user not connected

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,30 +3,18 @@ import { MapComponent } from '../components/map/map.component';
 import { MainMenuComponent } from './pages/menu/components/main-menu/main-menu.component';
 import { SettingsComponent } from './pages/menu/components/settings/settings.component';
 import { AuthPageComponent } from './pages/auth/components/auth-page/auth-page.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
+  { path: 'auth', component: AuthPageComponent },
   {
     path: '',
-    pathMatch: 'full',
-    component: AuthPageComponent,
-  },
-  {
-    path: 'menu',
-    pathMatch: 'full',
-    component: MainMenuComponent,
-  },
-  {
-    path: 'settings',
-    pathMatch: 'full',
-    component: SettingsComponent,
-  },
-  {
-    path: 'game',
-    pathMatch: 'full',
-    component: MapComponent,
-  },
-  {
-    path: '**',
-    component: MainMenuComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', component: MainMenuComponent },
+      { path: 'settings', component: SettingsComponent },
+      { path: 'game', component: MapComponent },
+      { path: '**', redirectTo: '' },
+    ],
   },
 ];
