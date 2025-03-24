3c8a622a4e543ee9891ce476eb72065be799bd54
IT-Misericordia-Chile <tichile@misericordia.fr>
2025-03-24 17:48:39 -0300

    show good username for players. Remove useless code

--- src/app/pages/ingame/components/player/player.component.ts
+++ src/app/pages/ingame/components/player/player.component.ts
@@ -1,37 +1,15 @@
-import {
-  Component,
-  ElementRef,
-  Input,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Player } from '../../services/player-service';
-import { UserService } from '../../../../../services/user.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss'],
 })
-export class PlayerComponent implements OnInit, OnDestroy {
+export class PlayerComponent {
   username: string = '';
-  private userSubscription!: Subscription;
   @Input() player!: Player;
 
   @ViewChild('playerCanvas', { static: true })
   canvasRef!: ElementRef<HTMLCanvasElement>;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.userSubscription = this.userService.user$.subscribe((user) => {
-      this.username = user.username;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.userSubscription.unsubscribe();
-  }
 }
